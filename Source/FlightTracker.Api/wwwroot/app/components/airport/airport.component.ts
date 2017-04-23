587af3e8f801df57486653ab8d98dfb4562a66d3
aberhamhaile <haile.31@osu.edu>
2017-04-23 19:47:16 -0400

    airport radio

--- Source/FlightTracker.Api/wwwroot/app/components/airport/airport.component.ts
+++ Source/FlightTracker.Api/wwwroot/app/components/airport/airport.component.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -14,21 +15,35 @@ export class AirportComponent  {
       constructor(private http: Http) {
     }
  
-    public getAirport(chosenAirport: string) {
-        this.http.get('/api/airports/' + chosenAirport).subscribe(result => {
+    public getAirport(chosenAirport: string, mySite: string) {
+        this.http.get('/api/airports/' + chosenAirport + '/' + mySite).subscribe(result => {
             this.airports = result.json();
         });
     }
 }
 
+
+
+   interface Item {
+        itemId: string;
+        airportID: string;
+        type: string;
+        name: string;
+        phone?: any;
+        address: string;
+        description: string;
+    }
 interface Airport {
-   airportId: string;
-  iataCode: string;
-  name: string;
-  address: string;
-  city: string;
-  state: string;
-  postalCode: number;
-  country: string;
-  items: Array<Array<string>>;
-}
+        airportId: string;
+        iataCode: string;
+        name: string;
+        address: string;
+        city: string;
+        state: string;
+        postalCode: string;
+        country: string;
+        items: Item[];
+    }
+
+
+
