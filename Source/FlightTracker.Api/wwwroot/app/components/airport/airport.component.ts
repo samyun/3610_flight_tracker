45ebafd03278e03fb362c56ed8aaceaccb104049
aberhamhaile <haile.31@osu.edu>
2017-04-24 11:07:25 -0400

    iterate multiple items

--- Source/FlightTracker.Api/wwwroot/app/components/airport/airport.component.ts
+++ Source/FlightTracker.Api/wwwroot/app/components/airport/airport.component.ts
@@ -1,32 +1,39 @@
 import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import {FormControl, FormGroup} from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
 
 @Component({
-  moduleId: module.id,
+    moduleId: module.id,
 
-  selector: 'airport',
-  templateUrl: 'airport.component.html'
+    selector: 'airport',
+    templateUrl: 'airport.component.html'
 })
-export class AirportComponent  {
- mySite: string='food';
-  public airports: Airport.RootObject;
-      constructor(private http: Http) {
+export class AirportComponent {
+    mySite: string = 'food';
+    public airports: Airport.RootObject;
+    keys = Object.keys(this.airports.items[0]);
+    constructor(private http: Http) {
     }
- 
+
     public getAirport(chosenAirport: string, mySite: string) {
-        this.http.get('/api/airports/' + chosenAirport + '/' + mySite ).subscribe(result => {
+        this.http.get('/api/airports/' + chosenAirport + '/' + mySite).subscribe(result => {
             this.airports = result.json();
         });
     }
-    
+
+        
+
+  
+
 }
 
 
-declare module Airport  {
+declare module Airport {
 
-    export interface Item  {
+    export interface Item {
         itemId: string;
         airportID: string;
         type: string;
@@ -36,7 +43,7 @@ declare module Airport  {
         description: string;
     }
 
-    export interface RootObject  {
+    export interface RootObject {
         airportId: string;
         iataCode: string;
         name: string;
